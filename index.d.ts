f66d3e343642d01bcd3fe3fcc12c387bde9166ee
JoshuaHall <joshuahallmail@gmail.com>
2020-05-06 13:31:46 -0500

    Made ports optional

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module "react-elm-components" {
     interface ElmProps {
         src: any;
-        ports: any;
+        ports?: any;
     }
 
     export = function (props: ElmProps): React.ReactElement<any> { }
